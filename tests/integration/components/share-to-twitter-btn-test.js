247690fe95f3820b8d60282c3257893f7b27f560
Harry <harrychenhc@icloud.com>
2021-04-09 16:59:42 -0400

    add ShareToTwitterBtn exists test

--- tests/integration/components/share-to-twitter-btn-test.js
+++ tests/integration/components/share-to-twitter-btn-test.js
@@ -7,20 +7,7 @@ module('Integration | Component | share-to-twitter-btn', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<ShareToTwitterBtn />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <ShareToTwitterBtn>
-        template block text
-      </ShareToTwitterBtn>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('[data-test=share-to-twitter-btn]').exists('ShareToTwitterBtn renders');
   });
 });
